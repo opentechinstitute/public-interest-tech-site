5275ac8235fe90df202433bec796d37486ca19d7
Maya Wagoner <mayawagon@gmail.com>
2016-12-28 23:42:46 -0800

    only renders if desc is between 20 and 100 characters (proxy for whether someone has reviewed entry)

--- app/Render.js
+++ app/Render.js
@@ -7,33 +7,38 @@ const render = {
     if (data.dead == true) {
       var ifdead = "dead";
     }
-    return `
-      <li class="result ${ifdead}">
-          <div class="result-preview">
-            <h2><span>${util.truncate(data.name, 30)}</span></h2>
-            <p class="result-description">${util.truncate(data.description, 132)}</p>
-          </div>
-          <div class="result-info ${ifdead}">
-              <h3><a href="${data.website ? data.website : '#'}" target="_blank">${data.name}</a> is ${data.whatisit} ${util.joinAnd(_.map(data.services, s => `<span class="result-service">${s.toLowerCase().replace('esl', 'ESL')}</span>`))}${data.additionalServices.length > 0 ? ' help' : ''}.</h3>
-              <div class="result-meta">
-                <div class="result-meta-info">
-                  ${data.state ? `<h5><span class="result-meta-lead">Based in </span><span class="result-state" data-state="${data.state}">${data.state}</span></h5>` : ''}
-                  <h5>${data.categories.join(', ')}</h5>
+    if (data.descriptionlength < 20 || data.descriptionlength > 100) {
+      return
+    }
+    else {
+      return `
+        <li class="result ${ifdead}">
+            <div class="result-preview">
+              <h2><span>${util.truncate(data.name, 30)}</span></h2>
+              <p class="result-description">${util.truncate(data.description, 132)}</p>
+            </div>
+            <div class="result-info ${ifdead}">
+                <h3><a href="${data.website ? data.website : '#'}" target="_blank">${data.name}</a> is ${data.whatisit} ${util.joinAnd(_.map(data.services, s => `<span class="result-service">${s.toLowerCase().replace('esl', 'ESL')}</span>`))}${data.additionalServices.length > 0 ? ' help' : ''}.</h3>
+                <div class="result-meta">
+                  <div class="result-meta-info">
+                    ${data.state ? `<h5><span class="result-meta-lead">Based in </span><span class="result-state" data-state="${data.state}">${data.state}</span></h5>` : ''}
+                    <h5>${data.categories.join(', ')}</h5>
+                  </div>
+                  <div class="result-meta-share">
+                    ${data.number ? `<a href="tel:${data.number}"><i class="fa fa-phone"></i></a>` : ''}
+                    <i class="fa fa-share-alt action-share" data-id="${data.id}"></i>
+                  </div>
                 </div>
-                <div class="result-meta-share">
-                  ${data.number ? `<a href="tel:${data.number}"><i class="fa fa-phone"></i></a>` : ''}
-                  <i class="fa fa-share-alt action-share" data-id="${data.id}"></i>
+                <div class="result-actions">
+                  ${data.donatelink ? `<a target="_blank" href="${data.donatelink}">Donate</a>` : ''}
+                  ${data.volunteerlink ? `<a target="_blank" href="${data.volunteerlink}">Volunteer</a>` : ''}
+                  ${data.contributelink ? `<a target="_blank" href="${data.contributelink}">Contribute</a>` : ''}
+                  ${data.joblink ? `<a target="_blank" href="${data.joblink}">Apply</a>` : ''}
                 </div>
-              </div>
-              <div class="result-actions">
-                ${data.donatelink ? `<a target="_blank" href="${data.donatelink}">Donate</a>` : ''}
-                ${data.volunteerlink ? `<a target="_blank" href="${data.volunteerlink}">Volunteer</a>` : ''}
-                ${data.contributelink ? `<a target="_blank" href="${data.contributelink}">Contribute</a>` : ''}
-                ${data.joblink ? `<a target="_blank" href="${data.joblink}">Apply</a>` : ''}
-              </div>
-          </div>
-          <div class="result-sharing"></div>
-      </li>`;
+            </div>
+            <div class="result-sharing"></div>
+        </li>`;
+    }
   },
 
   rating: function(rating) {
